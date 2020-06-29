9fdcddad9249bba7e8c24a67d34b395bee3fee54
Mikael Midtgaard <mikaelmidt@gmail.com>
2020-06-29 19:32:07 +0200

    More config

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const { SlackAdapter } = require('botbuilder-adapter-slack');
-const botkit = require('botkit');
-const dotenv = require('dotenv');
+const { SlackAdapter } = require("botbuilder-adapter-slack");
+const botkit = require("botkit");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const adapter = new SlackAdapter({
   clientSigningSecret: process.env.SLACK_SECRET,
-  botToken: process.env.SLACK_TOKEN
+  botToken: process.env.SLACK_TOKEN,
+  scopes: [
+    "app_mentions:read",
+    "channels:history",
+    "channels:join",
+    "channels:read",
+    "chat:write",
+    "chat:write.customize",
+    "commands",
+    "emoji:read",
+    "groups:history",
+    "groups:read",
+    "reactions:read",
+    "users:read",
+    "users:write",
+  ],
+  oauthVersion: "v2",
+  enable_incomplete: true, // TODO: remove after debugging
 });
 
 const controller = new botkit.Botkit({
@@ -15,14 +32,12 @@ const controller = new botkit.Botkit({
 
 controller.ready(() => console.log("Bot ready for action!"));
 
-controller.on('message', async(bot, message) => {
-  console.log("message:");
-  console.log(message.text);
-  await bot.reply(message, 'I heard a message!');
-});
-
-controller.on("direct_mention", (bot, message) => {
-  console.log("direct_mention:");
-  console.log(message.text);
+const messageHandler = (bot, message) => {
+  console.log(`message type ${message.type}: ${message.text}`);
   Promise.resolve().then(() => bot.reply(message, "hey man what's up"));
-});
+};
+
+controller.on("message_received", messageHandler);
+controller.on("direct_mention", messageHandler);
+controller.on("mention", messageHandler);
+controller.on("direct_message", messageHandler);
